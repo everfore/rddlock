08c5b0487194b60574c5fb5dca1e0c9f14075019
toukii <toukii@toukii.com>
2017-08-25 13:57:48 +0800

    lock-retry_AND_test

--- rdd.go
+++ rdd.go
@@ -17,17 +17,23 @@ func UnLock(rds redis.Cmdable, key string) bool {
 	return true
 }
 
-func Lock(rds redis.Cmdable, key string, timeout_ms, retry int) bool {
+func expiredTime(timeout_ms int) (int64, int64) {
+	now := time.Now()
+	return now.Unix(), now.Add(time.Millisecond).Unix()
+}
+
+func Lock(rds redis.Cmdable, key string, timeout_ms int) bool {
 	now, ex := expiredTime(timeout_ms)
 	setnxCmd := rds.SetNX(key, ex, 0)
-	// log.Printf("SetNX:%+v", setnxCmd.Val())
 	if setnxCmd.Val() {
+		// log.Println("setnx got a lock")
 		return true
 	}
 	getCmd := rds.Get(key)
 	if getCmd.Val() == "" {
 		getSetCmd := rds.GetSet(key, ex)
 		if getSetCmd.Val() == "" {
+			// log.Println("get nil, then getset then got a lock")
 			return true
 		}
 	} else {
@@ -41,18 +47,49 @@ func Lock(rds redis.Cmdable, key string, timeout_ms, retry int) bool {
 		if now > prevTime {
 			getSetCmd2 := rds.GetSet(key, ex)
 			if getSetCmd2.Val() == getCmd.Val() {
-				// log.Println("getset then got a locker.", getSetCmd2.Val())
+				// log.Println("timeout, getset then got a lock")
 				return true
 			}
 		} else {
 			return false
 		}
 	}
-	// log.Println("now:", now)
 	return false
 }
 
-func expiredTime(timeout_ms int) (int64, int64) {
-	now := time.Now()
-	return now.Unix(), now.Add(time.Millisecond).Unix()
+// 重试 retry_times 次
+func LockRetry(rds redis.Cmdable, key string, timeout_ms, retry_times int) bool {
+	for i := 0; i < retry_times; i++ {
+		now, ex := expiredTime(timeout_ms)
+		setnxCmd := rds.SetNX(key, ex, 0)
+		if setnxCmd.Val() {
+			// log.Println("setnx got a lock")
+			return true
+		}
+		getCmd := rds.Get(key)
+		if getCmd.Val() == "" {
+			getSetCmd := rds.GetSet(key, ex)
+			if getSetCmd.Val() == "" {
+				// log.Println("get nil, then getset then got a lock")
+				return true
+			}
+		} else {
+			// log.Println("get is not nil, peek timeout")
+			prevTime, err := getCmd.Int64()
+			if err != nil {
+				log.Println("get key int64 err:", err)
+			} else {
+				// 已经过期，可以尝试获得锁了
+				if now > prevTime {
+					getSetCmd2 := rds.GetSet(key, ex)
+					if getSetCmd2.Val() == getCmd.Val() {
+						// log.Println("timeout, getset then got a lock")
+						return true
+					}
+				}
+			}
+		}
+		time.Sleep(time.Millisecond)
+	}
+	return false
 }
--- rdd_test.go
+++ rdd_test.go
@@ -32,21 +32,22 @@ func redisClusterSetUp(addrs []string) {
 
 func init() {
 	redisSetUp("localhost", 32768, "", 0)
+	// redisClusterSetUp([]string{"192.168.199.1:11110", "192.168.199.1:11111", "192.168.199.1:11112"})
 }
 
 func TestLock(t *testing.T) {
 	start := time.Now()
-	locked := Lock(rds, "locker", 1000, 5)
+	locked := Lock(rds, "locker", 1000)
 	t.Log("locked:", locked)
 
 	unlocked := UnLock(rds, "locker")
 	t.Log("unlocked:", unlocked)
 	t.Logf("cost:%+v", time.Now().Sub(start))
 
-	locked = Lock(rds, "locker", 1000, 5)
+	locked = Lock(rds, "locker", 1000)
 	t.Log("locked:", locked)
 
-	locked = Lock(rds, "locker", 1000, 5)
+	locked = Lock(rds, "locker", 1000)
 	t.Log("locked2:", locked)
 
 	unlocked = UnLock(rds, "locker")
@@ -54,12 +55,13 @@ func TestLock(t *testing.T) {
 }
 
 func TestSync(t *testing.T) {
-	fmt.Println("TestSync")
 	all := int32(100)
 	counter := int32(0)
 	for i := 0; i < 200; i++ {
 		go func() {
-			locked := Lock(rds, "locker-for-all", 5, 1)
+			start := time.Now()
+			locked := Lock(rds, "locker-for-all", 5)
+			fmt.Printf("locked:%+v, cost %+v\n", locked, time.Now().Sub(start))
 			if locked {
 				// atomic.AddInt32(&counter, 1)
 				counter++
@@ -74,7 +76,7 @@ func TestSync(t *testing.T) {
 				fmt.Println("locked", counter, "-", all)
 			}
 		}()
-		time.Sleep(5e5)
+		time.Sleep(5e7)
 	}
 	if counter+all < 100 {
 		fmt.Println("ERROR")
@@ -82,23 +84,134 @@ func TestSync(t *testing.T) {
 	fmt.Printf("counter:%d, all:%d\n", counter, all)
 }
 
+/**
+[cmd] go test -v -test.run Time
+
+local-redis
+=== RUN   TestTime
+success:200, avg:1.1732105 ms
+failed:0, avg:NaN ms
+--- PASS: TestTime (10.53s)
+PASS
+ok  	github.com/everfore/rddlock	10.546s
+
+uat-redis
+=== RUN   TestTime
+success:200, avg:5.2903905 ms
+failed:0, avg:NaN ms
+--- PASS: TestTime (10.56s)
+PASS
+ok  	github.com/everfore/rddlock	10.839s
+*/
+// go test -v -test.run Time
+func TestTime(t *testing.T) {
+	var succTime, failedTime float32
+	var succCount, failedCount int
+	for i := 0; i < 200; i++ {
+		go func() {
+			start := time.Now()
+			locked := Lock(rds, "locker-for-all", 5)
+			cost := time.Now().Sub(start)
+			if locked {
+				succTime += float32(cost.Nanoseconds())
+				succCount++
+				defer UnLock(rds, "locker-for-all")
+			} else {
+				failedTime += float32(cost.Nanoseconds())
+				failedCount++
+			}
+		}()
+		time.Sleep(5e7)
+	}
+	fmt.Printf("success:%d, avg:%+v ms\n", succCount, succTime/(float32(succCount)*float32(1000000.0)))
+	fmt.Printf("failed:%d, avg:%+v ms\n", failedCount, failedTime/(float32(failedCount)*float32(1000000.0)))
+}
+
 func BenchmarkLock(b *testing.B) {
 	var lockCount int32
 	for i := 0; i < b.N; i++ {
 		go UnLock(rds, "key")
-		ok := Lock(rds, "key", 1, 1)
+		ok := Lock(rds, "key", 1)
 		if ok {
 			b.Log("locked")
 			atomic.AddInt32(&lockCount, 1)
 		}
 	}
 	time.Sleep(1e9)
-	ok := Lock(rds, "key", 1, 1)
+	ok := Lock(rds, "key", 1)
 	fmt.Println("locked:", ok)
 	if !ok {
 		UnLock(rds, "key")
 	}
-	ok = Lock(rds, "key", 1, 1)
+	ok = Lock(rds, "key", 1)
 	fmt.Println("again locked:", ok)
 	fmt.Printf("b.N:%d, locked:%d\n", b.N, lockCount)
+}
+
+func TestLockRetry(t *testing.T) {
+	lockKey := "lockRetry-key"
+	all := int32(100)
+	counter := int32(0)
+	for i := 0; i < 200; i++ {
+		go func() {
+			locked := LockRetry(rds, lockKey, 10, 10)
+			if locked {
+				counter++
+				if all > 0 {
+					all--
+					if counter+all != 100 {
+						fmt.Println("ERR")
+					}
+				}
+			}
+			fmt.Printf("locked:%+v, %d - %d\n", locked, counter, all)
+			defer UnLock(rds, lockKey)
+		}()
+		time.Sleep(time.Millisecond)
+	}
+	if counter+all < 100 {
+		fmt.Println("ERROR")
+	}
+}
+
+/**
+[cmd] go test -v -test.run RetryTime
+
+local-redis
+=== RUN   TestLockRetryTime
+success:200, avg:1.1741205 ms
+failed:0, avg:NaN ms
+--- PASS: TestLockRetryTime (10.58s)
+PASS
+ok  	github.com/everfore/rddlock	10.597s
+
+uat-redis
+=== RUN   TestLockRetryTime
+success:200, avg:12.572702 ms
+failed:0, avg:NaN ms
+--- PASS: TestLockRetryTime (10.59s)
+PASS
+ok  	github.com/everfore/rddlock	10.634s
+*/
+func TestLockRetryTime(t *testing.T) {
+	var succTime, failedTime float32
+	var succCount, failedCount int
+	for i := 0; i < 200; i++ {
+		go func() {
+			start := time.Now()
+			locked := LockRetry(rds, "locker-for-all", 10, 5)
+			cost := time.Now().Sub(start)
+			if locked {
+				succTime += float32(cost.Nanoseconds())
+				succCount++
+				defer UnLock(rds, "locker-for-all")
+			} else {
+				failedTime += float32(cost.Nanoseconds())
+				failedCount++
+			}
+		}()
+		time.Sleep(5e7)
+	}
+	fmt.Printf("success:%d, avg:%+v ms\n", succCount, succTime/(float32(succCount)*float32(1000000.0)))
+	fmt.Printf("failed:%d, avg:%+v ms\n", failedCount, failedTime/(float32(failedCount)*float32(1000000.0)))
 }
