4403577e25a6a072c387d111eef02f3b79137c63
toukii <toukii@toukii.com>
2017-08-25 10:08:19 +0800

    rdd-test:lock-unlock

--- rdd.go
+++ rdd.go
@@ -1,6 +1,9 @@
 package rddlock
 
 import (
+	"log"
+	"time"
+
 	redis "gopkg.in/redis.v5"
 )
 
@@ -8,7 +11,48 @@ func UnLock(rds redis.Cmdable, key string) bool {
 	cmd := rds.Del(key)
 	err := cmd.Err()
 	if err != nil {
+		log.Println(err)
 		return false
 	}
 	return true
+}
+
+func Lock(rds redis.Cmdable, key string, timeout_ms, retry int) bool {
+	now, ex := expiredTime(timeout_ms)
+	setnxCmd := rds.SetNX(key, ex, 0)
+	// log.Printf("SetNX:%+v", setnxCmd.Val())
+	if setnxCmd.Val() {
+		return true
+	}
+	getCmd := rds.Get(key)
+	if getCmd.Val() == "" {
+		getSetCmd := rds.GetSet(key, ex)
+		if getSetCmd.Val() == "" {
+			return true
+		}
+	} else {
+		// log.Println("get is not nil, peek timeout")
+		prevTime, err := getCmd.Int64()
+		if err != nil {
+			log.Println("get key int64 err:", err)
+			return false
+		}
+		// 已经过期，可以尝试获得锁了
+		if now > prevTime {
+			getSetCmd2 := rds.GetSet(key, ex)
+			if getSetCmd2.Val() == getCmd.Val() {
+				// log.Println("getset then got a locker.", getSetCmd2.Val())
+				return true
+			}
+		} else {
+			return false
+		}
+	}
+	// log.Println("now:", now)
+	return false
+}
+
+func expiredTime(timeout_ms int) (int64, int64) {
+	now := time.Now()
+	return now.Unix(), now.Add(time.Millisecond).Unix()
 }
--- rdd_test.go
+++ rdd_test.go
@@ -1,19 +1,23 @@
 package rddlock
 
 import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
 	"gopkg.in/ezbuy/redis-orm.v1/orm"
 	redis "gopkg.in/redis.v5"
-	"testing"
 )
 
-var _redis_store *orm.RedisStore
+var rds *orm.RedisStore
 
 func redisSetUp(host string, port int, password string, db int) {
 	store, err := orm.NewRedisClient(host, port, password, db)
 	if err != nil {
 		panic(err)
 	}
-	_redis_store = store
+	rds = store
 }
 
 func redisClusterSetUp(addrs []string) {
@@ -23,13 +27,78 @@ func redisClusterSetUp(addrs []string) {
 	if err != nil {
 		panic(err)
 	}
-	_redis_store = store
+	rds = store
 }
 
 func init() {
-	redisSetUp(host, port, password, db)
+	redisSetUp("localhost", 32768, "", 0)
 }
 
-func TestUnLock(t *testing.T) {
-	UnLock(_redis_store, "locker")
+func TestLock(t *testing.T) {
+	start := time.Now()
+	locked := Lock(rds, "locker", 1000, 5)
+	t.Log("locked:", locked)
+
+	unlocked := UnLock(rds, "locker")
+	t.Log("unlocked:", unlocked)
+	t.Logf("cost:%+v", time.Now().Sub(start))
+
+	locked = Lock(rds, "locker", 1000, 5)
+	t.Log("locked:", locked)
+
+	locked = Lock(rds, "locker", 1000, 5)
+	t.Log("locked2:", locked)
+
+	unlocked = UnLock(rds, "locker")
+	t.Log("unlocked:", unlocked)
+}
+
+func TestSync(t *testing.T) {
+	fmt.Println("TestSync")
+	all := int32(100)
+	counter := int32(0)
+	for i := 0; i < 200; i++ {
+		go func() {
+			locked := Lock(rds, "locker-for-all", 5, 1)
+			if locked {
+				// atomic.AddInt32(&counter, 1)
+				counter++
+				if all > 0 {
+					all--
+					// atomic.AddInt32(&all, -1)
+					if counter+all != 100 {
+						fmt.Println("ERR")
+					}
+				}
+				defer UnLock(rds, "locker-for-all")
+				fmt.Println("locked", counter, "-", all)
+			}
+		}()
+		time.Sleep(5e5)
+	}
+	if counter+all < 100 {
+		fmt.Println("ERROR")
+	}
+	fmt.Printf("counter:%d, all:%d\n", counter, all)
+}
+
+func BenchmarkLock(b *testing.B) {
+	var lockCount int32
+	for i := 0; i < b.N; i++ {
+		go UnLock(rds, "key")
+		ok := Lock(rds, "key", 1, 1)
+		if ok {
+			b.Log("locked")
+			atomic.AddInt32(&lockCount, 1)
+		}
+	}
+	time.Sleep(1e9)
+	ok := Lock(rds, "key", 1, 1)
+	fmt.Println("locked:", ok)
+	if !ok {
+		UnLock(rds, "key")
+	}
+	ok = Lock(rds, "key", 1, 1)
+	fmt.Println("again locked:", ok)
+	fmt.Printf("b.N:%d, locked:%d\n", b.N, lockCount)
 }
