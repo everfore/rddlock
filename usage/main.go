22dc6ccfde21a1a988ab3a52cf9f6876011268ab
toukii <toukii@toukii.com>
2017-08-26 12:15:51 +0800

    usage

--- usage/main.go
+++ usage/main.go
@@ -19,23 +19,30 @@ func main() {
 
 	lock_key := "lock-key"
 
-	locked := rddlock.Lock(rds, lock_key, 5)
+	locked, ex := rddlock.Lock(rds, lock_key, 5)
 	if locked {
 		fmt.Printf("LOCK %s: %+v\n", lock_key, locked)
-		rddlock.UnLock(rds, lock_key)
+		unlocked := rddlock.UnLock(rds, lock_key, ex)
+		if unlocked {
+			fmt.Printf("UNLOCK %s: %+v\n", lock_key, unlocked)
+		} else {
+			unlocked = rddlock.UnLockUnsafe(rds, lock_key)
+			fmt.Printf("UNLOCK-UNSAFE %s: %+v\n", lock_key, unlocked)
+		}
 	}
 
 	// retry lock
 
 	// 1. lock the key first
-	locked = rddlock.Lock(rds, lock_key, 5)
+	locked, _ = rddlock.Lock(rds, lock_key, 5)
 	fmt.Printf("FIRST step, LOCK %s:%+v\n", lock_key, locked)
 	// 2. retry to lock the locked key
-	locked = rddlock.LockRetry(rds, lock_key, 100, 100)
+	locked, _ = rddlock.LockRetry(rds, lock_key, 100, 100)
 	fmt.Printf("SECOND step, LOCK-RETRY %s:%+v\n", lock_key, locked)
 }
 
 // Output
 // LOCK lock-key: true
+// UNLOCK lock-key: true
 // FIRST step, LOCK lock-key:true
-// SECOND step, LOCK-RETRY lock-key:true/false
+// SECOND step, LOCK-RETRY lock-key:true
